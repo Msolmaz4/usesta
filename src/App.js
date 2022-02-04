391cb72f4bbba971018bcf10b3d8ce02027f0998
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-02-04 01:31:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+const [num, setNum] =useState(0)
+
+     
+const onArt =()=>{
+  return(
+    setNum(num + 1)
+  )
+}
+ const onAzalt =()=>{
+   return(
+     setNum(num-1)
+   )
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+<button onClick={onArt}>artir</button>
+<button onClick={onAzalt}>ayalt</button>
+   <p>{num}</p>
     </div>
   );
 }
